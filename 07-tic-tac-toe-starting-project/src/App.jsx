8538718be2c3e3252f4cd982eb10c8d659309b98
Autthawigorn <twiggyramirez8@gmail.com>
2025-10-09 21:47:44 +0700

    Lift State Up

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,22 +2,47 @@ import { useState } from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
-const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
 
-function handleSelectSquare() {
-  setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+ function handleSelectSquare(rowIndex, colIndex) {
+  setGameTurns((prevTurns) => {
+    let currentPlayer = "X";
+
+    if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+      currentPlayer = "O";
+    }
+
+    const updatedTurns = [
+      { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+      ...prevTurns,
+    ];
+
+    return updatedTurns;
+  });
+
+  setActivePlayer((curActivePlayer) =>
+    curActivePlayer === "X" ? "O" : "X"
+  );
 }
 
   return (
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player name="Player 1" symbol="X" isActive={activePlayer === "X"}/>
-        <Player name="Player 2" symbol="O" isActive={activePlayer === "O"}/>
-      </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+        />
+      </div>
+      <Log />
+    </main>
   );
 }
 export default App;
---
+++ 07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -0,0 +1,10 @@
+export default function Log() {
+  return (
+    <ol id="log">
+      <h2>Game Log</h2>
+      <ol>
+        {/* Game log entries will go here */}
+      </ol>
+    </ol>
+  );
+}
